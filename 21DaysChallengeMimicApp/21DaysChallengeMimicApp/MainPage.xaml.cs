807bd28b0a105dd63f0ab1f864d8bd1eca8b9c09
simhlakuva <simahlakuva@gmail.com>
2020-11-19 15:07:59 +0200

    Add project files.

---
+++ 21DaysChallengeMimicApp/21DaysChallengeMimicApp/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace _21DaysChallengeMimicApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            public TimeSpan TimeOfTheDay { get; }
+        }
+    }
+}
